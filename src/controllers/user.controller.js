4421500d9a294978691bccab806f9d14c9eb4169
Srishty11 <srishty361@gmail.com>
2024-07-06 14:12:14 +0530

    all set

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler( async (req,res) => {
-  res.status(200).json({
+  res.status(200).json({    // here 200 is http status code
         message: "Ok"
     })
 } )
